ce1d9c538b6924db1f20f7496127f7d6dd25ac90
Lexi <alexamapes13@gmail.com>
2022-08-11 13:06:35 -0400

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
 const removeFromArray = function() {
+    const args = Array.from(arguments);
 
+    let userArray = args[0];
+    let numOfArgs = args.length - 1;
+
+    for (let i = 1; i <= numOfArgs; i++) {
+        let element = args[i];
+        let elementIndex = userArray.indexOf(element);
+        
+        if (elementIndex == -1) {
+            continue;
+        }
+
+        userArray.splice(elementIndex, 1);
+    }
+
+    return userArray;
 };
 
 // Do not edit below this line
