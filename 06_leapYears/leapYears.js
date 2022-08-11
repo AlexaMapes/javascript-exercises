aa78556cc68752723034e99d81f844f5da868a13
Lexi <alexamapes13@gmail.com>
2022-08-11 15:42:21 -0400

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(userYear) {
+    if ((userYear % 4) == 0) {
+        if ((userYear % 100) == 0) {
+            if ((userYear % 400) == 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return true;
+        }
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
