f2257ab3e8ed1984d5f9d875b29ef3497bcb4a17
Lexi <alexamapes13@gmail.com>
2022-08-11 13:27:33 -0400

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let finalSum = 0;
 
+    if ((typeof num1 !== "number") || typeof num2 !== "number" || (num1 < 0) || (num2 < 0)) {
+        return "ERROR";
+    }
+
+    if (num1 > num2) {
+        let buffer = num1;
+        num1 = num2;
+        num2 = buffer;
+    }
+
+    for (i = num1; i <= num2; i++) {
+        finalSum = finalSum + i;
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
