1796d6bc9a6c5501965bcba739433c97d484c7d9
Lexi <alexamapes13@gmail.com>
2022-08-11 12:35:35 -0400

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let stringArray = text.split("");
+    let reversedArray = stringArray.reverse();
+    let reversedString = reversedArray.join("");
 
+    return reversedString;
 };
 
 // Do not edit below this line
